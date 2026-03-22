948c09ed8d5f4098d32718fcac0a59024ece021c
Daksh Sharma <daksharma5804@gmail.com>
2026-03-22 12:56:47 +0530

    Add Volt formula for API testing

---
+++ Formula/volt.rb
@@ -0,0 +1,25 @@
+class Volt < Formula
+  desc "Terminal Based Binary for API Testing"
+  homepage "https://github.com/frypan05/Volt"
+  version "1.0.0"
+  license "MIT"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/frypan05/Volt/releases/download/v#{version}/volt-x86_64-apple-darwin.tar.gz"
+      sha256 "PASTE_X86_64_HASH_HERE"
+    end
+    on_arm do
+      url "https://github.com/frypan05/Volt/releases/download/v#{version}/volt-aarch64-apple-darwin.tar.gz"
+      sha256 "PASTE_AARCH64_HASH_HERE"
+    end
+  end
+
+  def install
+    bin.install "volt"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/volt --version 2>&1", 1)
+  end
+end
