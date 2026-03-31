95974f6bd458f419c28c464eb6e3d4c646a13aa1
Daksh Sharma <daksharma5804@gmail.com>
2026-03-31 17:45:11 +0530

    Update SHA256 checksums for Volt binaries

--- Formula/volt.rb
+++ Formula/volt.rb
@@ -7,22 +7,22 @@ class Volt < Formula
   on_macos do
     on_intel do
       url "https://github.com/frypan05/Volt/releases/download/v#{version}/volt-x86_64-apple-darwin.tar.gz"
-      sha256 "0bbb3ca3db69837be5a26b770cfbb5d5b4fe321ac70c875742e159f0ee4570f9"
+      sha256 "8a1a8af98d2be45780df66a1ba80680da96e352ae98c862eec842138f10af61c"
     end
     on_arm do
       url "https://github.com/frypan05/Volt/releases/download/v#{version}/volt-aarch64-apple-darwin.tar.gz"
-      sha256 "fc92202d2e21379ada21effea9159c6f1f85241870f654158e95587e03193326"
+      sha256 "3b548fca1e209273c2c8ba1798e397f44ca2229b3105a8a6844cbf2c6de6dce0"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/frypan05/Volt/releases/download/v#{version}/volt-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "d8da305b01d53ae3a216b88fad80f4a48676e8919f94e4c2a95f6b7a51a4658e"
+      sha256 "0bbb3ca3db69837be5a26b770cfbb5d5b4fe321ac70c875742e159f0ee4570f9"
     end
     on_arm do
       url "https://github.com/frypan05/Volt/releases/download/v#{version}/volt-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "24a5f0e48b12ed43eb78c47e3f427b724b95f1211c949099d329d4b5dc3b3f36"
+      sha256 "578250f3050c3237f8058494e0711892cec8eb8f1edd5e952bbe18cd94e4e598"
     end
   end
 
